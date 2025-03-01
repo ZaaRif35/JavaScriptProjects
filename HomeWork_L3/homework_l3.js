5191c8f6092e351f4cc84d3496614be52c519dd8
ZaaRif35 <caramangabriel35@gmail.com>
2025-03-01 17:56:18 +0200

    Add files via upload

---
+++ HomeWork_L3/homework_l3.js
@@ -0,0 +1,9 @@
+function calculArieDreptunghi(latime, lungime) {
+    return latime * lungime;
+}
+
+let latimeDreptunghi = 5;
+let lungimeDreptunghi = 10;
+let arieDreptunghi = calculArieDreptunghi(latimeDreptunghi, lungimeDreptunghi);
+
+console.log("Aria dreptunghiului cu lățimea", latimeDreptunghi, "și lungimea", lungimeDreptunghi, "este:", arieDreptunghi);
